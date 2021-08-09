f497d76ec93d56209798d248dd310805d8ab1dfe
snuffy <41898282+github-actions[bot]@users.noreply.github.com>
2021-08-09 06:57:38 -0500

    try this

--- jupyter_client/_version.py
+++ jupyter_client/_version.py
@@ -1,4 +1,6 @@
 import re
+from typing import List
+from typing import Union
 
 __version__ = "7.0.0rc0"
 
@@ -6,7 +8,7 @@ __version__ = "7.0.0rc0"
 pattern = r'(?P<major>\d+).(?P<minor>\d+).(?P<patch>\d+)(?P<rest>.*)'
 match = re.match(pattern, __version__)
 if match:
-    parts = [int(match[part]) for part in ['major', 'minor', 'patch']]
+    parts: List[Union[int, str]] = [int(match[part]) for part in ['major', 'minor', 'patch']]
     if match['rest']:
         parts.append(match['rest'])
 else:
