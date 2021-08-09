643a5dd1c806ed4c8da29b0c003f281131e85dc2
snuffy <41898282+github-actions[bot]@users.noreply.github.com>
2021-08-09 06:41:47 -0500

    try this

--- jupyter_client/_version.py
+++ jupyter_client/_version.py
@@ -5,9 +5,12 @@ __version__ = "7.0.0rc0"
 # Build up version_info tuple for backwards compatibility
 pattern = r'(?P<major>\d+).(?P<minor>\d+).(?P<patch>\d+)(?P<rest>.*)'
 match = re.match(pattern, __version__)
-parts = [int(match[part]) for part in ['major', 'minor', 'patch']]
-if match['rest']:
-    parts.append(match['rest'])
+if match:
+    parts = [int(match[part]) for part in ['major', 'minor', 'patch']]
+    if match['rest']:
+        parts.append(match['rest'])
+else:
+    parts = ()
 version_info = tuple(parts)
 
 
